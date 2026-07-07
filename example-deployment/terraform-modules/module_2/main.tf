c5eddb1b106c6bc18b659362a8f028a381fd6052
stephen nadraus <stephen.nadraus.work@gmail.com>
2026-07-07 08:52:05 -0600

    chore: update output to use color

--- example-deployment/terraform-modules/module_2/main.tf
+++ example-deployment/terraform-modules/module_2/main.tf
@@ -3,6 +3,6 @@ resource "terraform_data" "echo" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "printf '\\033[34;1mModule_2 executed, remote value: ${var.value}.\\033[0m\\n'"
+    command = "printf '\\033[33;1mModule_2 executed, remote value: ${var.value}.\\033[0m\\n'"
   }
 }
